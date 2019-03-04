ae9c8739f24f616e31baf05663a91db6ba8f9760
lkarun <lalanke.karunaratne@research.ucla.edu>
2019-03-04 13:17:42 -0800

    Updates

---
+++ precache-manifest.cb6b6e39cde00439b86a60ee3af8547d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "8bc0856a4a3acfc18e58",
+    "url": "/react_contactmanager_datalist/static/js/runtime~main.8bc0856a.js"
+  },
+  {
+    "revision": "2be2fa8a81662bb50a9b",
+    "url": "/react_contactmanager_datalist/static/js/main.2be2fa8a.chunk.js"
+  },
+  {
+    "revision": "7374d7d0e41198065248",
+    "url": "/react_contactmanager_datalist/static/js/2.7374d7d0.chunk.js"
+  },
+  {
+    "revision": "2be2fa8a81662bb50a9b",
+    "url": "/react_contactmanager_datalist/static/css/main.11d832ee.chunk.css"
+  },
+  {
+    "revision": "7374d7d0e41198065248",
+    "url": "/react_contactmanager_datalist/static/css/2.091c3880.chunk.css"
+  },
+  {
+    "revision": "89f76de4985ff8147df2dc372e7895cd",
+    "url": "/react_contactmanager_datalist/index.html"
+  }
+];
