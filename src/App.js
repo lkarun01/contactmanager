4c8d74c5e865095aac9bd1045d76e1d350c35ce3
lkarun <lalanke.karunaratne@research.ucla.edu>
2019-03-04 13:09:02 -0800

    added async/await and edit contact feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Contacts from "./components/contatcs/Contacts";
 import AddContact from "./components/contatcs/AddContact";
+import EditContact from "./components/contatcs/EditContact";
 import Header from "./components/layout/Header";
-import About from './components/pages/about';
+import About from "./components/pages/about";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "./context";
 import NotFound from "./components/pages/notfound";
@@ -17,12 +18,17 @@ class App extends Component {
           <div className="App">
             <Header branding="Contact Manager" />
             <div className="container">
-            <Switch>
-              <Route exact path="/" component={Contacts} />
-              <Route exact path="/contacts/add" component={AddContact} />
-              <Route exact path="/about" component={About} />
-              <Route exact component={NotFound} />
-            </Switch>
+              <Switch>
+                <Route exact path="/" component={Contacts} />
+                <Route exact path="/contacts/add" component={AddContact} />
+                <Route
+                  exact
+                  path="/contacts/edit/:id"
+                  component={EditContact}
+                />
+                <Route exact path="/about" component={About} />
+                <Route exact component={NotFound} />
+              </Switch>
             </div>
           </div>
         </Router>
--- src/components/contatcs/EditContact.js
+++ src/components/contatcs/EditContact.js
@@ -3,7 +3,7 @@ import { Consumer } from "../../context";
 import TextInputGroup from "../layout/TextInputGroup";
 import axios from "axios";
 
-class AddContact extends Component {
+class EditContact extends Component {
   state = {
     name: "",
     email: "",
@@ -11,6 +11,19 @@ class AddContact extends Component {
     errors: {}
   };
 
+  async componentDidMount() {
+    const { id } = this.props.match.params;
+    const res = await axios.get(
+      `https://jsonplaceholder.typicode.com/users/${id}`
+    );
+    const contact = res.data;
+
+    this.setState({
+      name: contact.name,
+      email: contact.email,
+      phone: contact.phone
+    });
+  }
   onSubmit = async (dispatch, e) => {
     e.preventDefault();
     const { name, email, phone } = this.state;
@@ -28,18 +41,21 @@ class AddContact extends Component {
       this.setState({ errors: { phone: "Phone is required..." } });
       return;
     }
-    const newContact = {
+
+    const { id } = this.props.match.params;
+
+    const updateContact = {
       name,
       email,
       phone
     };
 
-    const res = await axios.post(
-      "https://jsonplaceholder.typicode.com/users",
-      newContact
+    const res = await axios.put(
+      `https://jsonplaceholder.typicode.com/users/${id}`,
+      updateContact
     );
 
-    dispatch({ type: "ADD_CONTACT", payload: res.data });
+    dispatch({ type: "UPDATE_CONTACT", payload: res.data });
 
     // Clear the fields
     this.setState({
@@ -64,7 +80,7 @@ class AddContact extends Component {
 
           return (
             <div className="card mb-3">
-              <div className="card-header">Add Contact</div>
+              <div className="card-header">Edit Contact</div>
               <div className="card-body">
                 <form onSubmit={this.onSubmit.bind(this, dispatch)}>
                   <TextInputGroup
@@ -95,7 +111,7 @@ class AddContact extends Component {
                   />
                   <input
                     type="submit"
-                    value="Add Contact"
+                    value="Update Contact"
                     className="btn btn-light btn-block"
                   />
                 </form>
@@ -108,4 +124,4 @@ class AddContact extends Component {
   }
 }
 
-export default AddContact;
+export default EditContact;
